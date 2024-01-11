d585da5ef0ec9ed7eceb5b98efb388abc2a71363
JustinBenning <justin.benning@gmail.com>
2024-01-11 15:51:02 -0500

    submission of assignment

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What is your favorite color?");
+            string favoriteColor = Console.ReadLine();
+            Console.WriteLine("What is your favorite Animal?");
+            string favoriteAnimal = Console.ReadLine();
+            Console.WriteLine("What is your favorite band?");
+            string favoriteBand = Console.ReadLine();
+            Console.WriteLine($"{name}'s favorite color is {favoriteColor}.\n That is their favorite color because their favorite animal is a {favoriteAnimal} and that is the color of their fur" +
+                $"\nTheir favorite band, {favoriteBand} sings a song about the same animal");
         }
     }
 }
